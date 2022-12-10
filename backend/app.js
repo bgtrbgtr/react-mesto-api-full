1f58d7f431d20c1e7007651ad9e80f302bd5a6c3
bgtrbgtr <jhmwmw@gmail.com>
2022-12-10 19:26:23 +0200

    Added public path to frontend

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,9 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
-const routes = require('./routes');
 
 const { PORT = 3001 } = process.env;
 const app = express();
@@ -23,7 +23,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 app.use(cookieParser());
-app.use(routes);
+app.use('/api', require('./routes'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
