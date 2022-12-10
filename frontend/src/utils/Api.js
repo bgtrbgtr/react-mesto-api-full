7e1d867fe28f618ff3f75b8d16d29df94b3b9ff6
bgtrbgtr <jhmwmw@gmail.com>
2022-12-10 19:30:22 +0200

    Added public path to frontend

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -104,7 +104,7 @@ class Api {
 }
 
 export const api = new Api({
-  baseUrl: "http://localhost:3001",
+  baseUrl: "http://lackluster-party.students.nomoredomains.club",
   headers: {
     "Content-Type": "application/json",
   },
